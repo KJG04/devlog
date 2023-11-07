37e7e333b590788a9e4f77f357bcf68ea6be5543
KJG04 <freedom7113@gmail.com>
2023-11-08 01:02:53 +0900

    [feat] 버그 수정

--- src/atoms/navigationBarVisible.ts
+++ src/atoms/navigationBarVisible.ts
@@ -1,6 +1,7 @@
 import { atom } from 'recoil';
+import { v1 } from 'uuid';
 
 export const navigationBarVisibleAtom = atom<boolean>({
   default: true,
-  key: 'navigationBarVisible',
+  key: `navigationBarVisible/${v1()}`,
 });
