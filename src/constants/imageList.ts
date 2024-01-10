7ec82d74344db676b0cecb88e4b9fb834b8ef193
KJG04 <freedom7113@gmail.com>
2024-01-11 03:34:36 +0900

    [Feat] 메시지 주고받기 글 작성

--- src/constants/imageList.ts
+++ src/constants/imageList.ts
@@ -4,6 +4,7 @@ import AfterCodeSplit from 'public/post-images/after-code-split.png'
 import PhaserBundleSize from 'public/post-images/phaser-bundle-size.png'
 import CodeSplitSlackMessage from 'public/post-images/code-split-slack-message.png'
 import RouterCodeSplitThumbnail from 'public/post-images/router-code-split-thumbnail.jpg'
+import MessageBetweenAppAndWebThumbnail from 'public/post-images/message-between-app-and-web-thumbnail.jpg'
 import Thumbnail20221017 from 'public/post-images/thumbnail-2022-10-17.webp'
 import Thumbnail20230401 from 'public/post-images/thumbnail-2023-04-01.webp'
 import Thumbnail20230402 from 'public/post-images/thumbnail-2023-04-02.webp'
@@ -18,6 +19,7 @@ const IMAGE_LIST = {
   PhaserBundleSize,
   CodeSplitSlackMessage,
   RouterCodeSplitThumbnail,
+  MessageBetweenAppAndWebThumbnail,
 }
 
 export const isInImageList = (str: string): str is keyof typeof IMAGE_LIST =>
