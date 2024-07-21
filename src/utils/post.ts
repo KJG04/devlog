527bde59a48e2b06a4df9576046862d3fc9be1a5
KJG04 <freedom7113@gmail.com>
2024-07-22 02:19:35 +0900

    feat: 글 캐시

--- src/utils/post.ts
+++ src/utils/post.ts
@@ -14,6 +14,8 @@ import { glob } from 'glob'
 import dayjs from 'dayjs'
 import rehypeMetaAsAttributes from '#utils/rehypeMetaAsAttributes'
 import p from 'path'
+import { unstable_cache } from 'next/cache'
+import CACHE_KEY from '#constants/cacheKey'
 
 const DIR_REPLACE_STRING = p.normalize('/src/posts')
 const POST_PATH = p.normalize(`${process.cwd()}${DIR_REPLACE_STRING}`)
@@ -30,7 +32,7 @@ function getFileNameByPath(path: string) {
   return fileNameWithoutExt
 }
 
-export const getAllPaths = async (): Promise<Path[]> => {
+export const getAllPaths = unstable_cache(async (): Promise<Path[]> => {
   const paths = glob.sync(`${POST_PATH}/**/*.md*`)
 
   return paths
@@ -42,46 +44,47 @@ export const getAllPaths = async (): Promise<Path[]> => {
       const params = { name: getFileNameByPath(path) }
       return { params }
     })
-}
+}, CACHE_KEY.GET_ALL_PATHS)
 
-export const getPostByPath = async (
-  path: string,
-): Promise<PostWithHTMLBody> => {
-  const fullPath = `${POST_PATH}/${path}.mdx`
-  const file = fs.readFileSync(fullPath, { encoding: 'utf8' })
+export const getPostByPath = unstable_cache(
+  async (path: string): Promise<PostWithHTMLBody> => {
+    const fullPath = `${POST_PATH}/${path}.mdx`
+    const file = fs.readFileSync(fullPath, { encoding: 'utf8' })
 
-  const { attributes, body } = frontMatter<FrontMatter>(file)
-  attributes.date = dayjs(attributes.date).toISOString()
+    const { attributes, body } = frontMatter<FrontMatter>(file)
+    attributes.date = dayjs(attributes.date).toISOString()
 
-  return {
-    frontMatter: attributes,
-    body: await serialize(body, {
-      mdxOptions: {
-        remarkPlugins: [remarkMath, toc, remarkGfm],
-        rehypePlugins: [
-          slug,
-          rehypeKatex,
-          rehypeMetaAsAttributes,
-          [prism, { showLineNumbers: true }],
-          [
-            rehypeAutolinkHeadings,
-            {
-              behavior: 'append',
-              properties: {
-                class: 'autolink-header',
-                ariaHidden: true,
-                tabIndex: -1,
+    return {
+      frontMatter: attributes,
+      body: await serialize(body, {
+        mdxOptions: {
+          remarkPlugins: [remarkMath, toc, remarkGfm],
+          rehypePlugins: [
+            slug,
+            rehypeKatex,
+            rehypeMetaAsAttributes,
+            [prism, { showLineNumbers: true }],
+            [
+              rehypeAutolinkHeadings,
+              {
+                behavior: 'append',
+                properties: {
+                  class: 'autolink-header',
+                  ariaHidden: true,
+                  tabIndex: -1,
+                },
+                content: [h('span.visually-hidden', ' #')],
               },
-              content: [h('span.visually-hidden', ' #')],
-            },
+            ],
           ],
-        ],
-      },
-    }),
-  }
-}
+        },
+      }),
+    }
+  },
+  CACHE_KEY.GET_POST_BY_PATH,
+)
 
-export const getAllPosts = async (): Promise<Post[]> => {
+export const getAllPosts = unstable_cache(async (): Promise<Post[]> => {
   const paths = glob.sync(`${POST_PATH}/**/*.md*`)
 
   return paths
@@ -98,57 +101,61 @@ export const getAllPosts = async (): Promise<Post[]> => {
       }
     })
     .filter((value) => value.frontMatter.published)
-}
+}, CACHE_KEY.GET_ALL_POSTS)
 
-export const getSeriesPosts = async (series: string): Promise<Post[]> => {
-  const posts = (await getAllPosts()).sort((a, b) => {
-    const dateA = dayjs(a.frontMatter.date)
-    const dateB = dayjs(b.frontMatter.date)
+export const getSeriesPosts = unstable_cache(
+  async (series: string): Promise<Post[]> => {
+    const posts = (await getAllPosts()).sort((a, b) => {
+      const dateA = dayjs(a.frontMatter.date)
+      const dateB = dayjs(b.frontMatter.date)
 
-    return dateA.isBefore(dateB) ? -1 : 1
-  })
+      return dateA.isBefore(dateB) ? -1 : 1
+    })
 
-  return posts
-    .filter((post) => post.frontMatter.series === series)
-    .map((item) => ({
-      ...item,
-      frontMatter: {
-        ...item.frontMatter,
-        date: dayjs(item.frontMatter.date).toISOString(),
-      },
-    }))
-}
+    return posts
+      .filter((post) => post.frontMatter.series === series)
+      .map((item) => ({
+        ...item,
+        frontMatter: {
+          ...item.frontMatter,
+          date: dayjs(item.frontMatter.date).toISOString(),
+        },
+      }))
+  },
+  CACHE_KEY.GET_SERIES_POSTS,
+)
 
-export const getNextPost = async (
-  currPost: FrontMatter,
-): Promise<Post | null> => {
-  const currSeries = currPost.series
+export const getNextPost = unstable_cache(
+  async (currPost: FrontMatter): Promise<Post | null> => {
+    const currSeries = currPost.series
 
-  if (!currSeries) {
-    return null
-  }
-
-  const posts = await getSeriesPosts(currSeries)
-  const currPostIndex = posts.findIndex(
-    (item) => item.frontMatter.date === currPost.date,
-  )
-
-  if (currPostIndex >= 0) {
-    const nextPostIndex = currPostIndex + 1
-
-    if (nextPostIndex < posts.length) {
-      return posts[nextPostIndex]
+    if (!currSeries) {
+      return null
     }
-  }
 
-  return null
-}
+    const posts = await getSeriesPosts(currSeries)
+    const currPostIndex = posts.findIndex(
+      (item) => item.frontMatter.date === currPost.date,
+    )
 
-export const getRecentPosts = async () => {
+    if (currPostIndex >= 0) {
+      const nextPostIndex = currPostIndex + 1
+
+      if (nextPostIndex < posts.length) {
+        return posts[nextPostIndex]
+      }
+    }
+
+    return null
+  },
+  CACHE_KEY.GET_NEXT_POST,
+)
+
+export const getRecentPosts = unstable_cache(async () => {
   return (await getAllPosts()).sort((a, b) => {
     const dateA = dayjs(a.frontMatter.date)
     const dateB = dayjs(b.frontMatter.date)
 
     return dateA.isBefore(dateB) ? 1 : -1
   })
-}
+}, CACHE_KEY.GET_RECENT_POSTS)
