95a1d8ec447891eb93cbb73e2b97d9dfb8d52929
KJG04 <freedom7113@gmail.com>
2024-09-26 01:51:06 +0900

    feat: 교육 추가

--- src/containers/about/components/EducationSection.tsx
+++ src/containers/about/components/EducationSection.tsx
@@ -13,17 +13,25 @@ const slideAnimationProps = {
 
 const EducationSection = () => {
   return (
-    <section className="m-auto max-w-screen-lg px-4">
-      <motion.h1 className="text-6xl font-black" {...slideAnimationProps}>
-        Education
-      </motion.h1>
-      <div className="mt-16" />
-      <Activity
-        title="대덕소프트웨어마이스터고등학교"
-        subtitle="소프트웨어개발과"
-        date="2020. 03. ~ 2023. 02."
-      />
-    </section>
+    <>
+      <section className="m-auto max-w-screen-lg px-4">
+        <motion.h1 className="text-6xl font-black" {...slideAnimationProps}>
+          Education
+        </motion.h1>
+        <div className="mt-16" />
+        <Activity
+          title="대덕소프트웨어마이스터고등학교"
+          subtitle="소프트웨어개발과"
+          date="2020. 03. ~ 2023. 02."
+        />
+        <div className="mt-16" />
+        <Activity
+          title="스파르타 코딩클럽 KDT 실무형 스프링 백엔드 엔지니어 양성과정 6회차"
+          subtitle="Spring 백엔드 개발"
+          date="2024. 08. ~ 2024. 11."
+        />
+      </section>
+    </>
   )
 }
 
